dcabeb24ccf280e1d99af2dedf4156663720d882
Nikita P. Shupeyko <webyneter@gmail.com>
2017-06-20 15:57:59 +0300

    Fix get_class_qualname command tests

--- python_humble_utils/tests/test_commands.py
+++ python_humble_utils/tests/test_commands.py
@@ -12,7 +12,7 @@ from ..commands import extract_file_name_with_extension, extract_file_dir_path, 
     generate_random_file_name_with_extension, parse_tuple_from_string, read_file, \
     create_or_update_file, camel_or_pascal_case_to_snake_case, get_all_subclasses, \
     camel_or_pascal_case_to_space_delimited, generate_random_dir_path, yield_file_paths, generate_hex_uuid_4, \
-    get_class_name
+    get_class_name, get_class_qualname
 from ..conftest import FileMeta
 from ..pytest_commands import generate_tmp_file_path
 
@@ -206,5 +206,5 @@ def test_when_getting_class_name_given_valid_arguments_should_succeed():
 
 
 def test_when_getting_class_qualname_given_valid_arguments_should_succeed():
-    assert get_class_name(Foo) == Foo.__class__.__qualname__
-    assert get_class_name(Foo.InsideFoo) == Foo.InsideFoo.__class__.__qualname__
+    assert get_class_qualname(Foo) == Foo.__qualname__
+    assert get_class_qualname(Foo.InsideFoo) == Foo.InsideFoo.__qualname__
