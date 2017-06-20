15218cb0bb5d5a308c21eef73cd7c642f2a76927
Nikita P. Shupeyko <webyneter@gmail.com>
2017-06-20 13:34:40 +0300

    Wipe out CLI

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import os
-from typing import Sequence
-
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -11,47 +8,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-
-# region requirements
-# Source: https://github.com/celery/celery/blob/master/setup.py
-
-def parse_requirements(*paths: str) -> Sequence[str]:
-    return [req for subreq in _requirements(*paths) for req in subreq]
-
-
-def _requirements(*paths: str) -> list:
-    return [
-        _pip_requirement(r)
-        for r in
-        (
-            _strip_comment(l)
-            for l in
-            open(os.path.join(os.getcwd(), 'requirements', *paths)).readlines()
-        )
-        if r
-    ]
-
-
-def _pip_requirement(requirement: str) -> Sequence[str]:
-    if requirement.startswith('-r '):
-        _, path = requirement.split()
-        return parse_requirements(*path.split('/'))
-    return [requirement]
-
-
-def _strip_comment(s):
-    return s.split('#', 1)[0].strip()
-
-
-install_requirements = [
-    'Click>=6.0',
-]
-
 setup_requirements = [
     'pytest-runner',
 ]
-
-# endregion
 
 
 setup(
@@ -67,19 +26,17 @@ setup(
 
     packages=find_packages(include=['python_humble_utils']),
     include_package_data=True,
-    install_requires=install_requirements,
 
     test_suite='python_humble_utils.tests',
-    tests_require=parse_requirements('test.txt'),
+
+    setup_requires=setup_requirements,
+    zip_safe=False,
 
     entry_points={
         'console_scripts': [
             'python_humble_utils=python_humble_utils.cli:main'
         ]
     },
-
-    setup_requires=setup_requirements,
-    zip_safe=False,
 
     license="MIT",
     classifiers=[
