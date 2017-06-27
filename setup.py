3448067c8cf6b433f830856f680cd46ca89d7bca
Nikita P. Shupeyko <webyneter@gmail.com>
2017-06-27 14:45:31 +0300

    Fix setup.py classifiers

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: ',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
